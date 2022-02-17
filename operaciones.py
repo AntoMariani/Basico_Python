60a2a783d72afa3e3d493de3db838a0671b0e92e
Antonella <antomaariani@gmail.com>
2022-02-16 22:18:19 -0300

    2022-03-16 - Commit inicial practicas Python

---
+++ operaciones.py
@@ -0,0 +1,39 @@
+print("""Se pueden hacer operaciones de manera simple en Python.\n
+Para sumar se usa el simbolo '+'
+5 + 5""")
+print(5+5)
+
+print("""\nPara restar se usa el simbolo '-'
+5 - 5""")
+print(5-5)
+
+print("""\nPara multiplicar se usa el simbolo '*'
+5 * 5""")
+print(5*5)
+
+print("""\nPara obtener el resultado de una división se usa el simbolo '/'
+5 / 5""")
+print(5/5)
+
+print("""\nPara obtener solo el cociente de una división se usa los simbolos '//'
+5 // 5""")
+print(5//5)
+
+print("""\nPara obtener el resto de una división se usa el simbolo '%'
+5 % 5""")
+print(5%5)
+
+print("""\nPara realizar una potencia '**'
+2 ** 5""")
+print(2**5)
+
+print("""\nPara realizar una raiz cuadrada sin importar nada se puede realizar con el siguiente operador '**' de la siguiente manera
+9 ** (0.5)""")
+print(9 ** (0.5))
+
+print("""\nRecorda que Python obedece la precedencia de operadores.
+Es decir que si le pedimos que realice: 5 * 5 + 2
+Lo realizará de la siguiente manera:
+(5 * 5) + 2
+10 + 2""")
+print(5*5+2)
